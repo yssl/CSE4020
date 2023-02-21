afce7d29b7b7d80e6e51ff3cb0d0e0a220fad2e8
Yoonsang Lee <yslee111@gmail.com>
2023-02-21 14:06:15 +0900

    Change window title

--- 3-Lab-SendingDataToShaders/1-colored-triangle.py
+++ 3-Lab-SendingDataToShaders/1-colored-triangle.py
@@ -87,7 +87,7 @@ def main():
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE) # for macOS
 
     # create a window and OpenGL context
-    window = glfwCreateWindow(800, 600, '3-hello-triangle', None, None)
+    window = glfwCreateWindow(800, 600, '1-colored-triangle', None, None)
     if not window:
         glfwTerminate()
         return
