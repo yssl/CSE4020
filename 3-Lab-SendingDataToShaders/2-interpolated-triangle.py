90b36b4ac41b1fec3c8fb84a39f5234548d7e7c1
Yoonsang Lee <yslee111@gmail.com>
2023-02-21 15:10:10 +0900

    Change window title

--- 3-Lab-SendingDataToShaders/2-interpolated-triangle.py
+++ 3-Lab-SendingDataToShaders/2-interpolated-triangle.py
@@ -89,7 +89,7 @@ def main():
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE) # for macOS
 
     # create a window and OpenGL context
-    window = glfwCreateWindow(800, 600, '1-colored-triangle', None, None)
+    window = glfwCreateWindow(800, 600, '2-interpolated-triangle', None, None)
     if not window:
         glfwTerminate()
         return
